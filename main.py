201f722ac820447537c88ad20ccf51d5d3ddc982
Kacper Florianski <36332694+thecodesummoner@users.noreply.github.com>
2019-03-01 21:16:34 +0000

    Added controller support in main execution

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import communication.data_manager as dm
 from communication.connection import Connection
+from control.controller import Controller
 
 if __name__ == "__main__":
-    s = Connection(dm)
-    #s = Connection(dm, ip="169.254.147.140", port=50001)
+    Controller(dm).init()
+    s = Connection(dm)#, ip="169.254.147.140", port=50001)
     s.connect()
