491c9581e0ea25b55a9a14ab4a12aca789269641
Kacper Florianski <36332694+thecodesummoner@users.noreply.github.com>
2019-03-04 20:03:04 +0000

    Removed debug code

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import communication.data_manager as dm
 from communication.connection import Connection
 from control.controller import Controller
-from time import sleep
 
 if __name__ == "__main__":
     c = Connection(dm)#, ip="169.254.147.140", port=50001)
     Controller(dm).init()
     c.connect()
-    while True:
-        print("Data from main module: ", dm.get_data())
-        pass
