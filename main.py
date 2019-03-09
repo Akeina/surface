9e69446bea7e15c73af7d07564f0e9ab30a4443d
Kacper Florianski <36332694+thecodesummoner@users.noreply.github.com>
2019-03-09 23:17:26 +0000

    Expanded example main

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ import communication.data_manager as dm
 from communication.connection import Connection
 from control.controller import Controller
 
+
 if __name__ == "__main__":
-    c = Connection(dm)#, ip="169.254.147.140", port=50001)
-    Controller(dm).init()
-    c.connect()
+
+    # Clear the cache on start
+    dm.clear()
+
+    # Initialise the connection
+    connection = Connection()#, ip="169.254.147.140", port=50001)
+
+    # Initialise the controller
+    controller = Controller()
+
+    # Start the tasks
+    connection.connect()
+    controller.init()
