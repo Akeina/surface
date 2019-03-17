0a255b73b43bfbab6661e221d9b94790161571a7
Kacper Florianski <36332694+thecodesummoner@users.noreply.github.com>
2019-03-17 16:38:53 +0000

    Added test scripts

--- main.py
+++ main.py
@@ -1,19 +1,49 @@
+# TODO: Remove unused imports
 import communication.data_manager as dm
 from communication.connection import Connection
+from communication.video_stream import VideoStream
 from control.controller import Controller
+from cv2 import imshow, waitKey
+
+
+# TODO: Remove this test script
+def sample_video_stream():
+    while True:
+        frame = video_stream.frame
+        try:
+            imshow("frame", frame)
+            waitKey(1)
+        except:
+            pass
+
+
+# TODO: Remove this test script
+def sample_text_debug():
+    while True:
+        print(dm.get_data())
 
 
 if __name__ == "__main__":
+
+    # Initialise the ip
+    ip = 'localhost' #ip = "169.254.147.140"
 
     # Clear the cache on start
     dm.clear()
 
     # Initialise the connection
-    connection = Connection()#, ip="169.254.147.140", port=50001)
+    connection = Connection(ip=ip)
+
+    # Initialise the video stream
+    video_stream = VideoStream(ip=ip)
 
     # Initialise the controller
     controller = Controller()
 
     # Start the tasks
     connection.connect()
+    video_stream.connect()
     controller.init()
+
+    # TODO: Remove this test script
+    sample_video_stream()
